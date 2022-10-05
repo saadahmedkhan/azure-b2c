c6bf2f23aab377bb774e20a3472dc6266e92b4d9
saad@revonic.com <saad@revonic.com>
2022-10-05 21:03:39 +0400

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
+import { ViteMinifyPlugin } from "vite-plugin-minify";
+import path from "path";
+import glob from "glob";
+
+export default defineConfig({
+    plugins: [viteSingleFile(), ViteMinifyPlugin({})],
+    root: path.resolve(__dirname, "src"),
+    build: {
+        //ssr: true,
+        outDir: path.resolve(__dirname, "dist"),
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                index: path.resolve(__dirname, "src", "index.html")
+            },
+            //input: glob.sync(path.resolve(__dirname, "src", "*.html"))
+            output: {
+                inlineDynamicImports: false
+            }
+        }
+    }
+})
